219f1c498464927869d5c663d98f1ee8bbdbeb6c
AabhasAgrawal <73350120+AabhasAgrawal@users.noreply.github.com>
2021-02-10 17:09:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+const Matter=matter.js;
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+}
+
+function draw() {
+  background(255,255,255);
+
+  
+  for ( var v = 40 ; v <=width;j=j+50 ){
+
+plinkos.push(new Plinkos(j,75))
+
+  }
+
+  
+  drawSprites();
+}
